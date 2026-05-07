fa15f0a03d3b9fd6d8b8e57faefa9cc3e2e8e631
NAIM WAKKAS <naimwakkasps4@gmail.com>
2026-05-07 21:52:36 +0200

    Configuración de GitHub Actions para despliegue automático

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Barbershop', // Reemplaza esto con el nombre exacto de tu repositorio si es diferente
 };
 
 export default nextConfig;
